2a83370d72cd7ad940260bef5d68b6c432ac40ce
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-05-31 21:23:07 +0200

    add license, prepare pom for maven central

--- src/main/java/ch/digitalfondue/vatchecker/EUVatChecker.java
+++ src/main/java/ch/digitalfondue/vatchecker/EUVatChecker.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright © 2018 digitalfondue (info@digitalfondue.ch)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package ch.digitalfondue.vatchecker;
 
 import org.w3c.dom.Document;
@@ -19,6 +34,7 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 /**
@@ -159,6 +175,8 @@ public class EUVatChecker {
      * @return
      */
     public static EUVatCheckResponse doCheck(String countryCode, String vatNumber, BiFunction<String, String, InputStream> documentFetcher) {
+        Objects.requireNonNull(countryCode, "countryCode cannot be null");
+        Objects.requireNonNull(vatNumber, "vatNumber cannot be null");
         try {
             String body = prepareTemplate(countryCode, vatNumber);
             try (InputStream is = documentFetcher.apply(ENDPOINT, body); Reader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
