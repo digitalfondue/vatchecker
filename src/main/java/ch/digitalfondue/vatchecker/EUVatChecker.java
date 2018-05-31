eb250dffb452eeb6007306146d274d55d91a8128
Sylvain Jermini <sylvain.jermini@syjer.com>
2018-05-31 20:19:40 +0200

    cleanup, add test

--- src/main/java/ch/digitalfondue/vatchecker/EUVatChecker.java
+++ src/main/java/ch/digitalfondue/vatchecker/EUVatChecker.java
@@ -11,6 +11,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.xpath.*;
@@ -18,18 +19,16 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.util.function.BiFunction;
 
+/**
+ * A small utility for calling the VIES webservice. See http://ec.europa.eu/taxation_customs/vies/ .
+ *
+ * The main entry points are {@link #doCheck(String, String)} and if more customization is needed {@link #doCheck(String, String, BiFunction)}.
+ */
 public class EUVatChecker {
 
-
-    private static String SOAP_CALL_TEMPLATE = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-            "<soapenv:Header/>" +
-            "<soapenv:Body>" +
-            "<checkVat xmlns=\"urn:ec.europa.eu:taxud:vies:services:checkVat:types\">" +
-            "<countryCode></countryCode><vatNumber></vatNumber>" +
-            "</checkVat>" +
-            "</soapenv:Body>" +
-            "</soapenv:Envelope>";
+    private static final Document BASE_DOCUMENT_TEMPLATE;
 
     private static final String ENDPOINT = "http://ec.europa.eu/taxation_customs/vies/services/checkVatService";
     private static final XPathExpression VALID_ELEMENT_MATCHER;
@@ -42,16 +41,40 @@ public class EUVatChecker {
             VALID_ELEMENT_MATCHER = xPath.compile("//checkVatResponse/valid");
             NAME_ELEMENT_MATCHER = xPath.compile("//checkVatResponse/name");
             ADDRESS_ELEMENT_MATCHER = xPath.compile("//checkVatResponse/address");
+
+            String soapCallTemplate = "<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+                    "<soapenv:Header/>" +
+                    "<soapenv:Body>" +
+                    "<checkVat xmlns=\"urn:ec.europa.eu:taxud:vies:services:checkVat:types\">" +
+                    "<countryCode></countryCode><vatNumber></vatNumber>" +
+                    "</checkVat>" +
+                    "</soapenv:Body>" +
+                    "</soapenv:Envelope>";
+
+            BASE_DOCUMENT_TEMPLATE = toDocument(new StringReader(soapCallTemplate));
+
         } catch (XPathExpressionException e) {
             throw new IllegalStateException(e);
         }
     }
 
     private static String prepareTemplate(String countryCode, String vatNumber) {
-        Document doc = toDocument(new StringReader(SOAP_CALL_TEMPLATE));
+        Document doc = copyDocument(BASE_DOCUMENT_TEMPLATE);
         doc.getElementsByTagName("countryCode").item(0).setTextContent(countryCode);
         doc.getElementsByTagName("vatNumber").item(0).setTextContent(vatNumber);
         return fromDocument(doc);
+    }
+
+    private static Document copyDocument(Document document) {
+        try {
+            Transformer tx   = TransformerFactory.newInstance().newTransformer();
+            DOMSource source = new DOMSource(document);
+            DOMResult result = new DOMResult();
+            tx.transform(source,result);
+            return (Document)result.getNode();
+        } catch (TransformerException e) {
+            throw new IllegalStateException(e);
+        }
     }
 
     private static Document toDocument(Reader reader) {
@@ -77,25 +100,52 @@ public class EUVatChecker {
         }
     }
 
-    public static EUVatCheckResponse doCheck(String countryCode, String vatNumber) {
-        try {
-            String body = prepareTemplate(countryCode, vatNumber);
-            URL url = new URL(ENDPOINT);
 
+    private static InputStream doCall(String endpointUrl, String document) {
+        try {
+            URL url = new URL(endpointUrl);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "text/xml;charset=UTF-8");
             conn.setDoOutput(true);
-            conn.getOutputStream().write(body.getBytes(StandardCharsets.UTF_8));
+            conn.getOutputStream().write(document.getBytes(StandardCharsets.UTF_8));
             conn.getOutputStream().flush();
+            return conn.getInputStream();
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
-            try (InputStream is = conn.getInputStream(); Reader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
+    /**
+     * Do a call to the EU vat checker web service.
+     *
+     * @param countryCode 2 character ISO country code. Note: Greece is EL, not GR.
+     * @param vatNumber the vat number to check
+     * @return
+     */
+    public static EUVatCheckResponse doCheck(String countryCode, String vatNumber) {
+        return doCheck(countryCode, vatNumber, EUVatChecker::doCall);
+    }
+
+    /**
+     * See {@link #doCheck(String, String)}. This method accept a documentFetcher if you need to customize the
+     * http client.
+     *
+     * @param countryCode 2 character ISO country code. Note: Greece is EL, not GR.
+     * @param vatNumber the vat number to check
+     * @param documentFetcher the function that, given the url of the web service and the body to post, return the resulting body as InputStream
+     * @return
+     */
+    public static EUVatCheckResponse doCheck(String countryCode, String vatNumber, BiFunction<String, String, InputStream> documentFetcher) {
+        try {
+            String body = prepareTemplate(countryCode, vatNumber);
+            try (InputStream is = documentFetcher.apply(ENDPOINT, body); Reader isr = new InputStreamReader(is, StandardCharsets.UTF_8)) {
                 Document result = toDocument(isr);
                 Node validNode = (Node) VALID_ELEMENT_MATCHER.evaluate(result, XPathConstants.NODE);
                 if (validNode != null) {
                     Node nameNode = (Node) NAME_ELEMENT_MATCHER.evaluate(result, XPathConstants.NODE);
                     Node addressNode = (Node) ADDRESS_ELEMENT_MATCHER.evaluate(result, XPathConstants.NODE);
-                    return new EUVatCheckResponse("true".equals(validNode.getTextContent()), nameNode.getTextContent(), addressNode.getTextContent());
+                    return new EUVatCheckResponse("true".equals(textNode(validNode)), textNode(nameNode), textNode(addressNode));
                 } else {
                     return new EUVatCheckResponse(false, null, null);
                 }
@@ -103,5 +153,9 @@ public class EUVatChecker {
         } catch (IOException | XPathExpressionException e) {
             throw new IllegalStateException(e);
         }
+    }
+
+    private static final String textNode(Node node) {
+        return node != null ? node.getTextContent() : null;
     }
 }
---
+++ src/test/java/ch/digitalfondue/vatchecker/EUVatCheckerTest.java
@@ -0,0 +1,32 @@
+package ch.digitalfondue.vatchecker;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class EUVatCheckerTest {
+
+    @Test
+    public void testSuccess() {
+        EUVatCheckResponse resp = EUVatChecker.doCheck("IT", "00950501007");
+        Assert.assertEquals(true, resp.isValid());
+        Assert.assertEquals("BANCA D'ITALIA", resp.getName());
+        Assert.assertEquals("VIA NAZIONALE 91 \n00184 ROMA RM\n", resp.getAddress());
+    }
+
+    @Test
+    public void testCorrectCountryCodeButInvalidVAT() {
+        EUVatCheckResponse resp = EUVatChecker.doCheck("IT", "00950501000");
+        Assert.assertEquals(false, resp.isValid());
+        Assert.assertEquals("---", resp.getName());
+        Assert.assertEquals("---", resp.getAddress());
+    }
+
+    @Test
+    public void testIncorrectCountry() {
+        EUVatCheckResponse resp = EUVatChecker.doCheck("AB", "009505010075353");
+        Assert.assertEquals(false, resp.isValid());
+        Assert.assertEquals(null, resp.getName());
+        Assert.assertEquals(null, resp.getAddress());
+    }
+}
